864aefe0b4ab314da0a06859610de01693e5f72f
Dylan Patch <dpatch15@gmail.com>
2020-10-02 19:45:00 -0600

    Finished first bit of pyBank before extra challenges

--- Python/pybank/main.py
+++ Python/pybank/main.py
@@ -1,6 +1,7 @@
 ##Import my dependencies
 import os
 import csv
+import statistics
 
 ##Create path to find data
 csv_path = "Resources/budget_data.csv"
@@ -8,10 +9,28 @@ csv_path = "Resources/budget_data.csv"
 print('Financial Analysis')
 print('-----------------------------')
 with open(csv_path) as csvfile:
-    totalMonths = sum(1 for line in csvfile)
-    print(f"Total Months on Record: {totalMonths}")
+    i = 0
     csvreader = csv.reader(csvfile, delimiter=",")
     net_gain = 0
+    dateArray = []
+    profitArray = []
+    biggestGain = 0
+    biggestLoss = 0
+    next(csvreader)
     for row in csvreader:
-        net_gain = net_gain + row[1]
-    print(net_gain)
+        i = i + 1
+        dateArray.append(row[0])
+        profitArray.append(int(row[1]))
+        if int(row[1]) > int(profitArray[biggestGain]):
+            biggestGain = i - 1
+        if int(row[1]) < int(profitArray[biggestLoss]):
+            biggestLoss = i - 1
+
+
+
+    
+    print(f"Total Months on Record: {len(profitArray)}")
+    print(f"Net profit/losses over period: {sum(profitArray)}")
+    print(f"Average change: {round(statistics.mean(profitArray),2)}")
+    print(f"Largest gain over period: {profitArray[biggestGain]} ({dateArray[biggestGain]})")
+    print(f"Largest loss over period: {profitArray[biggestLoss]} ({dateArray[biggestLoss]})")
