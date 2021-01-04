460b76463f13139b4f014fdeb3ecc425141937ad
Dylan Patch <dpatch15@gmail.com>
2021-01-04 10:41:15 -0700

    Added readme file

---
+++ Javascript/README.md
@@ -0,0 +1,11 @@
+# UFO sightings 
+A simple creation to use javascript to dynamically update table data to filter ufo sighting data.
+
+## The Problem
+With the nature of ufo sighting reporting there ends up being too many data points to filter and observe. 
+
+## The solution
+Using javascript I was able to identify elements on the html page to update the data displayed based upon desired filters.
+
+### Moving forward
+Through this project I was able to better understand how powerful using multiple languages and technologies together can be. I would love to invest more time in cleaning up the css, adding dropdowns that autopopulate with cities and countries in the data, and include a data range filter.
--- Javascript/static/js/app.js
+++ Javascript/static/js/app.js
@@ -1,8 +1,10 @@
-// from data.js
+// Get data from data.js
 var tableData = data;
 
+// Get table body from html to work with
 var tbody = d3.select("#ufo-table-body");
 
+//Function to display desired data
 function makeTable(ufoData){
     tbody.html('');
     ufoData.forEach(function(sighting){
@@ -14,9 +16,10 @@ function makeTable(ufoData){
     });
 };
 
+// Starts the page with all data from data.js
 makeTable(tableData);
 
-
+// Function to filter dependent on what is entered
  function filterTime() {
     d3.event.preventDefault();
     var fullFilter = tableData;
@@ -46,10 +49,11 @@ makeTable(tableData);
         fullFilter = fullFilter.filter(entry => entry.shape === shapeFilter);
         console.log(fullFilter);
     }
+    //End of function calls function to create table based on what data is left after filter
     makeTable(fullFilter);
 };
 
-
+// Calls filter function on form submission
 var filterButton = d3.select('#filter-btn');
 var form = d3.select('#form');
 filterButton.on("click", filterTime);
